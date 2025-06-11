415700c36b8bfadfd0ac8b0e405d62f19cab24ee
Carolina Pugan <lola17g@gmail.com>
2025-06-11 16:21:43 -0300

    criando seções

---
+++ src/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const button = document.querySelectorAll('[data-tab-button]');
+    
+    
+    for (let i = 0; i < button.length; i++) {
+        button[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            abaAlvo.classList.add('shows__list--is-active');
+        })
+    }
+})
+
+function escondeTodasAbas () {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
